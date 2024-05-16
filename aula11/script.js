eced285403cd73ec291c31442a450250e8ff4946
Guilherme Simões <guilherme97simoes97@gmail.com>
2024-05-16 21:58:01 +0100

    aula 11

---
+++ aula11/script.js
@@ -0,0 +1,11 @@
+window.onload = () => {
+    const webGallery = document.querySelector("web-gallery");
+    webGallery.addEventListener("ready", (event) => {
+        console.log("gallery ready", event.detail.numberOfimages);
+    })
+    webGallery.addEventListener("play-pause", (event) => {
+        console.log("gallery playing", event.detail.isPlaying);
+    })
+    webGallery.currentItem = 2;
+    webGallery.dataURL = "assets/gallery_data.json";
+}
